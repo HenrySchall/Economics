b540282b2058b49897ffbdca0ec96d6faf56be38
Henrique Werner <henrique.schall@outlook.com>
2025-02-24 03:53:29 -0300

    .

--- 1.r
+++ 1.r
@@ -1,80 +1,31 @@
-############################
-### IMPORTAÇÃO DOS DADOS ###
-############################
+###########################
+### ANALISANDO OS DADOS ###
+###########################
+
 # Tipo de dado que queremos no R
 # - ts: série temporal
 # - zoo: objeto zoo 
 # - xts: no formato xts
 # - timeSeries: série financeira
 
-# Configurando API (https://data.nasdaq.com/publishers/QDL) -> criar sua própria API Key
-Quandl.api_key("iVtrK8_YHwXzG9pA4mmQ")
-
-###########
-### SGS ###
-###########
-
-dados <- gbcbd_get_series(id = 4380, first.date = "2000-01-01",last.date = Sys.Date())
-View(dados)
-dygraph(dados, main = "Título") %>% dyRangeSelector()
-
-# Criando série
-dados_x <- dados$"value"
-serie <- ts(dados_x, frequency = 12, star= c(2000), end=(2004))
-title("Título")
-plot(serie)
-
-# Se tivermos uma base tratada, uma outra alterntiva seria transformar dataframe em vetor.
-# dados_vec <- as.vector(t(dados))
-# print(dados_vec)
-
-############
-### IPEA ###
-############
-
-series_ipeadata <- available_series()
-filter(series_ipeadata,str_detect(source, regex("caged", ignore_case = TRUE)))
-View(series_ipeadata)
-
-#Selecionando a série desejada
-glp <- ipeadata("ANP_CGASN")
-View(glp)
-
-dados_x <- glp$"value"
-gnp_serie <- ts(dados_x, frequency = 12, star= c(2005))
-plot(gnp_serie)
-
-#################
-### Excel/CSV ###
-#################
-
-getwd() #representa o diretório de trabalho atual do processo R
-setwd("C:/Users/henri/OneDrive/Repositórios")
-
-dados <- read.csv2("C:/Users/henri/OneDrive/Repositórios/R/Introdução/Datasets/chuva_mensal_sp.csv",
-sep = ";", header=TRUE, encoding="UTF-8")
-
-dados <- read_excel("C:/Users/henri/OneDrive/Repositórios/R/Introdução/Datasets/chuva_mensal_sp.xls")
+dados <- import(file="https://analisemacro.com.br/download/38675/",format = "csv",setclass = "tbl_df")
 View(dados)
 
-# header = cabeçario
-# stringsAsFactors = considerar todas as caracteres como fatores
-# sep = separando as variáveis 
-# fileEncoding = "UTF-8" or "latin1"
-# read.csv -> o default é sep=”,” e dec=”.”
-# read.csv2 -> o default é sep=”;” e dec=”,”.
+class(dados)
+str(dados)
 
 #####################
-### Yahoo Finance ###
+### Deflacionando ###
 #####################
 
-microsoft <- getSymbols("MSFT", src = "yahoo", auto.assign = FALSE, return.class = 'xts')
-View(microsoft)
-plot(microsoft)
+mutate(dados,real = (inpc[data == "2021-12-01"] / inpc) * salario_minimo)
 
-# Open: O preço de abertura nas datas especificadas
-# High: O preço da alta nas datas especificadas
-# Low: O preço da baixa nas datas especificadas
-# Close: O preço de fechamento nas datas especificadas
-# Volume: O volume nas datas especificadas
-# Adjusted: O preço de fechamento ajustado depois de aplicar distribuições de dividendos ou divisão da ação.
+# Salvar em um novo objeto
+dados_reais <- mutate(dados,real = (inpc[data == "2021-12-01"] / inpc) * salario_minimo)
+
+# Salvar no mesmo objeto
+dados <- mutate(dados,real = (inpc[data == "2021-12-01"] / inpc) * salario_minimo)
+
+##########################
+### Visualizando dados ###
+##########################
