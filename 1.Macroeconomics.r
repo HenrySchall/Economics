c2d391cec0b08e53405903c84851a98cf93ac205
Henrique Werner <henrique.schall@outlook.com>
2025-02-24 04:07:25 -0300

    .

--- 1.Macroeconomics.r
+++ 1.Macroeconomics.r
@@ -26,6 +26,12 @@ dados_reais <- mutate(dados,real = (inpc[data == "2021-12-01"] / inpc) * salario
 # Salvar no mesmo objeto
 dados <- mutate(dados,real = (inpc[data == "2021-12-01"] / inpc) * salario_minimo)
 
-##########################
-### Visualizando dados ###
-##########################
+####################################
+### Visualizando dados (ggplot2) ###
+####################################
+
+ggplot(data = dados) + # inicia a primeira camada do gráfico
+aes(x = data) + # define o eixo X
+geom_line(aes(y=salario_minimo,color = "Nominal"),size = 1) + # plota uma linha com as observações                      
+geom_line(aes(y=real,color = "Real"),size = 1) + # adiciona a outra linha de observações                 
+labs(title = "Salário mínimo - Brasil",subtitle = "Valores deflacionados pelo INPC a preços de dez/2021",x = "Data",y = "R$",color = NULL, caption  = "Dados: IBGE/IPEADATA") # define títulos/textos do gráfico
