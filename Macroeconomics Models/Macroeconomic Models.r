3d26e95fc817ed8a13407282111de9d1534df141
Henrique Werner <henrique.schall@outlook.com>
2025-02-28 02:38:39 -0300

    .

---
+++ Macroeconomics Models/Macroeconomic Models.r
@@ -0,0 +1,22 @@
+library(magrittr)
+library(rio)      
+library(timetk)   
+library(dplyr) 
+library(tidyverse)   
+library(tidyr)    
+library(ggplot2) 
+library(readr)
+library(readxl)
+library(forecast)
+library(zoo)
+library(rio) 
+library(timetk)
+library(lubridate)
+
+# if the packages weren't installed, run the commands in R Terminal -> https://github.com/HenrySchall/Data_Science/blob/main/R/Install_Packages.txt
+
+# Tipo de dados 
+# - ts: série temporal
+# - zoo: objeto zoo 
+# - xts: no formato xts
+# - timeSeries: série financeira
