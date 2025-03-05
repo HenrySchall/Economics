9a4bc83ee46394b384095fda53b60179443fb6e6
Henrique Werner <henrique.schall@outlook.com>
2025-03-05 08:55:13 -0300

    .

---
+++ 1.Introdução/3.X-13ARIMA-SEATS.r
@@ -0,0 +1,21 @@
+library(magrittr)
+library(rio)      
+library(timetk)   
+library(dplyr) 
+library(tidyverse)   
+library(tidyr)    
+library(ggplot2) 
+library(readr)
+library(readxl)
+library(forecast)
+library(zoo)
+library(lubridate)
+library(ipeadatar) 
+library(sidrar)
+library(GetBCBData) 
+
+# if some packages weren't installed, run the commands in R Terminal -> https://github.com/HenrySchall/Data_Science/blob/main/R/Install_Packages.txt
+
+#######################################################
+### X-13ARIMA-SEATS (Seasonal Adjustment Program) #####
+#######################################################
