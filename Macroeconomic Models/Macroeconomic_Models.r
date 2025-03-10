5b415b83f31f8326bf5450ea564c2587b7a7fb34
Henrique Werner <henrique.schall@outlook.com>
2025-03-10 14:02:24 -0300

    .

--- Macroeconomic Models/Macroeconomic_Models.r
+++ Macroeconomic Models/Macroeconomic_Models.r
@@ -1,8 +1,3 @@
-if(!require(magrittr)){install.packages("magrittr")}
-if(!require(mFilter)){install.packages("mFilter")}
-if(!require(rio)){install.packages("rio")}
-if(!require(BCDating)){install.packages("BCDating")}
-
 library(magrittr)
 library(rio)      
 library(timetk)   
@@ -31,56 +26,31 @@ dados <- import(file = "https://drive.google.com/uc?export=download&id=1vJ1l_1hi
 mutate(ln_pib = log(pib), tempo  = row_number())
 
 # Vetor de cores
-cores <- c( "#282f6b","#b22200","#eace3f","#224f20")
-g1 <- dados %>% ggplot() + aes(x = data, y = pib, color = "PIB") +  # especificação de eixos e título da série
-geom_line(size = 1) +                    # adiciona linha com observações
-scale_color_manual(values = cores) +     # define cor da(s) linha(s)
-theme(legend.position = "bottom") +      # posiciona a legenda embaixo
-labs(title = "PIB do Brasil", subtitle = "Preços de mercado, nº índice sazonalmente ajustado (média de 1995 = 100)", y = "Índice", x = NULL, color = NULL, caption  = "Dados: IBGE | Elaboração: analisemacro.com.br")
+cores <- c( "#0ba773","#b22200","#eace3f","#11c1ce")
+g1 <- dados %>% ggplot() + aes(x = data, y = pib, color = "PIB") +  
+geom_line(size = 1) +               
+scale_color_manual(values = cores) + # define cor da(s) linha(s)
+theme(legend.position = "bottom", panel.background = element_rect(fill = "white"), plot.background = element_rect(fill = "gray")) +
+labs(title = "PIB do Brasil", subtitle = "Preços de mercado, nº índice sazonalmente ajustado (média de 1995 = 100)", y = "Índice", x = NULL, color = NULL, caption  = "Dados: IBGE")
 
-Plot(g1) 
+plot(g1)
 
-
-# Tendência linear --------------------------------------------------------
+########################
+### Tendência Linear ###
+########################
 
 # Regressão linear do PIB contra o tempo
-reg1 <- lm(
-  formula = ln_pib ~ tempo,   # especificação do modelo no formato de fórmula
-  data    = dados             # fonte dos dados
-  )
+reg1 <- lm(formula = ln_pib ~ tempo, data = dados)
+potencial_tl <- reg1 %>% fitted() %>% exp()       
 
-# Salva a tendência estimada
-potencial_tl <- reg1 %>%
-  fitted() %>% # extrai os valores estimados
-  exp()        # reverte a transformação logarítmica
+# fitted() %>% -> extrai os valores estimados
+# exp() -> reverte a transformação logarítmica
 
-# Atualiza gráfico base com nova linha da tendência
-g1 +
-  ggplot2::geom_line(
-    mapping = ggplot2::aes(y = potencial_tl, color = "Tendência linear"),
-    size    = 1
-    )
+g1 + geom_line(mapping = aes(y = potencial_tl, color = "Tendência linear"), size = 1)
 
-
-# Tendência quadrática ----------------------------------------------------
-
-# Regressão linear do PIB contra o tempo + tempo^2  (veja help de I)
-reg2 <- lm(formula = ln_pib ~ tempo + I(tempo^2), data = dados)
-
-# Salva a tendência estimada
-potencial_tq <- reg2 %>%
-  fitted() %>% # extrai os valores estimados
-  exp()        # reverte a transformação logarítmica
-
-# Atualiza gráfico base com nova linha da tendência
-g1 +
-  ggplot2::geom_line(
-    mapping = ggplot2::aes(y = potencial_tq, color = "Tendência quadrática"),
-    size    = 1
-    )
-
-
-# Filtro HP ---------------------------------------------------------------
+######################################
+### Filtro Hodrick e Prescott (HP) ### 
+######################################
 
 # Converte dados para classe ts
 dados_ts <- ts(                         # função para criar um objeto de classe ts
@@ -108,8 +78,9 @@ g1 +
     size    = 1
     )
 
-
-# Filtro de Hamilton ------------------------------------------------------
+##########################
+### Filtro de Hamilton ###
+##########################
 
 # Regressão linear aplicando a especificação de Hamilton
 reg3 <- lm(
@@ -133,16 +104,15 @@ g1 +
     size    = 1
     )
 
-
-
-# Calculando o hiato ------------------------------------------------------
+#####################################
+### Calculando o hiato do produto ### 
+#####################################
 
 # Cálculo do hiato do produto
 hiato <- dados %>%
   dplyr::select("data", "pib") %>% # seleciona colunas de interesse
   dplyr::mutate(                   # cria novas colunas com cálculo do hiato
     `Tendência Linear`     = (pib / potencial_tl - 1) * 100,
-    `Tendência Quadrática` = (pib / potencial_tq - 1) * 100,
     `Filtro HP`            = (pib / potencial_hp - 1) * 100,
     `Filtro de Hamilton`   = (pib / potencial_h - 1) * 100
     ) %>%
@@ -214,3 +184,141 @@ plot(x = ciclo, y = dados_ts, main = "Businees Cycle Brazil")
 par(bg = "gray", fg = "black") 
 plot(x = ciclo, y = dados_ts, , col = "black", col.main = "black", col.axis = "black", col.lab = "black", border = "black", lwd = 1, main = "Ciclo de negócios brasileiro (PIB)",
 sub = "Produzidos por:", xlab = "Anos", ylab = "Frequência")
+
+####################
+### Sollow Model ###
+####################
+
+
+# Funções Definidas pelo Usuário ------------------------------------------
+
+# Função que calcula a renda per capita no estado estacionário de Solow
+y_ss <- function(A, s, n, g, delta, alpha){
+
+  y <- A * (s / (n + g + delta))^(alpha / (1-alpha))
+
+  return(y)
+
+}
+
+
+# Valores padrão para variáveis exógenas do modelo
+A     <- 1
+s     <- 0.1
+n     <- 0.01
+g     <- 0.015
+delta <- 0.05
+alpha <- 0.33
+
+
+# Simulação 1: choque na taxa de crescimento populacional -----------------
+
+# Criar um vetor numérico com uma sequência simulada de choques (novos valores
+# de n)
+choque_n <- seq(from = -0.05, to = 0, by = 0.001)
+
+
+# Calcular efeito em y no estado estacionário dado choques em n
+efeito_n <- y_ss(A = A, s = s, n = choque_n, g = g, delta = delta, alpha = alpha)
+
+
+# Gerar gráfico da renda per capita vs. choque em n
+dplyr::tibble(y = efeito_n, n = choque_n) %>%
+  ggplot2::ggplot() +
+  ggplot2::aes(x = n, y = y) +
+  ggplot2::geom_line(size = 2, color = "#282f6b") +
+  ggplot2::labs(
+    title    = "Estado estacionário de Solow: \nefeito de choque em n sobre a renda per capita",
+    subtitle = "Assumindo que: A = 1, s = 10%, g = 1,5%, delta = 5% e alpha = 0,33",
+    y        = "Renda per capita (y)",
+    x        = "Taxa de crescimento populacional (n)",
+    caption  = "Elaboração: analisemacro.com.br"
+    )
+
+
+
+# Simulação 2: choque na taxa de poupança ---------------------------------
+
+# Criar um vetor numérico com uma sequência simulada de choques (novos valores
+# de s)
+choque_s <- seq(from = 0.1, to = 0.5, by = 0.01)
+
+
+# Calcular efeito em y no estado estacionário dado choques em s
+efeito_s <- y_ss(A = A, s = choque_s, n = n, g = g, delta = delta, alpha = alpha)
+
+
+# Gerar gráfico da renda per capita vs. choque em s
+dplyr::tibble(y = efeito_s, s = choque_s) %>%
+  ggplot2::ggplot() +
+  ggplot2::aes(x = s, y = y) +
+  ggplot2::geom_line(size = 2, color = "#282f6b") +
+  ggplot2::labs(
+    title    = "Estado estacionário de Solow: \nefeito de choque em s sobre a renda per capita",
+    subtitle = "Assumindo que: A = 1, n = 1%, g = 1,5%, delta = 5% e alpha = 0,33",
+    y        = "Renda per capita (y)",
+    x        = "Taxa de poupança (s)",
+    caption  = "Elaboração: analisemacro.com.br"
+    )
+
+
+
+# Simulação 3: choque tecnológico -----------------------------------------
+
+# Criar um vetor numérico com uma sequência simulada de choques (novos valores
+# de A)
+choque_A <- seq(from = 1.05, to = 2.5, by = 0.01)
+
+
+# Calcular efeito em y no estado estacionário dado choques em A
+efeito_A <- y_ss(A = choque_A, s = s, n = n, g = g, delta = delta, alpha = alpha)
+
+
+# Gerar gráfico da renda per capita vs. choque em A
+dplyr::tibble(y = efeito_A, A = choque_A) %>%
+  ggplot2::ggplot() +
+  ggplot2::aes(x = A, y = y) +
+  ggplot2::geom_line(size = 2, color = "#282f6b") +
+  ggplot2::labs(
+    title    = "Estado estacionário de Solow: \nefeito de choque em A sobre a renda per capita",
+    subtitle = "Assumindo que: s = 10%, n = 1%, g = 1,5%, delta = 5% e alpha = 0,33",
+    y        = "Renda per capita (y)",
+    x        = "Nível tecnológico (A)",
+    caption  = "Elaboração: analisemacro.com.br"
+    )
+
+
+
+# Extra -------------------------------------------------------------------
+
+# Calculando o capital per capita dado a renda per capita:
+# Note que y = k^alpha, então -> k = y^(1/alpha)
+# Uma vez obtido o k, podemos obter também a reta de depreciação (n + g + delta)k
+# e a curva de investimento (sy)
+solow <- dplyr::tibble(y = efeito_s, s = choque_s) %>%
+  dplyr::mutate(
+    k           = y^(1/alpha),
+    n_g_delta_k = (n + g + delta) * k,
+    sy          = mean(s) * y # por simplificação, média dos choques em s
+    )
+
+# Plotando o diagrama de Solow
+solow %>%
+  ggplot2::ggplot() +
+  ggplot2::aes(x = k) +
+  ggplot2::geom_line(mapping = ggplot2::aes(y = y, color = "y = k^alpha")) +
+  ggplot2::geom_line(mapping = ggplot2::aes(y = n_g_delta_k, color = "(n + g + delta)k")) +
+  ggplot2::geom_line(mapping = ggplot2::aes(y = sy, color = "sy")) +
+  ggplot2::labs(
+    title    = "Diagrama de Solow: estado estacionário",
+    subtitle = paste0(
+      "Assumindo que: A = 1, s = ", mean(solow$s),
+      ", n = 1%, g = 1,5%, delta = 5% e alpha = 0,33"
+      ),
+    y        = NULL,
+    color    = NULL,
+    x        = "Capital per capita (k)",
+    caption  = "Elaboração: analisemacro.com.br"
+    )
+
+
